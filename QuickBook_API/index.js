a52b78e2c97320d62eccb20185ddde1c58376daf
jilsjb <jilsjb>
2024-02-07 19:27:40 +0400

    package added

--- QuickBook_API/index.js
+++ QuickBook_API/index.js
@@ -1,1 +1,9 @@
-const http = requier('http');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello mi');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('started');
+});
